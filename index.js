7caefe4232452aef86c409df5a947366f7e111dc
John Crespi Oncher <johnoncher@gmail.com>
2018-11-29 04:20:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 
-
 function theBeatlesPlay(musicians, instruments){
-  var fun = [];
-  for (musicians
-  
+  var music = [];
+  for (var i= 0; i < musicians.length;i++){
+    music.push(musicians[i] + " plays " + instruments[i]); 
+  }
+  return music;
+}
+
+function johnLennonFacts(facts){
+  var y =0;
+  while(y < facts.length){
+    facts[] += '!!!';
+    y++;
+  }
+  return facts
 }
